f627bfb761386f43bd8575216c30f639f1679379
Juan R. Gavilanes <jrgavilanes@gmail.com>
2017-12-04 14:17:34 +0100

    base

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,30 @@
+import { Component} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent {
+
+  heroe: Heroe;
+
+  constructor( private activatedRoute: ActivatedRoute,
+               private heroeService: HeroesService) {
+
+    //console.log('lupas2');
+    this.activatedRoute.params.subscribe( params => {
+      //console.log(params['id']);
+
+      //console.log(heroeService.getHeroe(params['id']));
+      this.heroe = heroeService.getHeroe(params['id']);
+
+    });
+
+
+  }
+
+
+
+}
